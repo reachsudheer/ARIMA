55733ac6b2c2b477115524421eb9f116e1adf5d7
Sudheer Tulluri <sudheer.hny@gmail.com>
2025-07-30 23:50:25 +0530

    added arima function

--- utils.py
+++ utils.py
@@ -3,3 +3,10 @@ import numpy as np
 from datetime import datetime
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.arima.model import ARIMA
+
+
+def arima_model(data,order):
+    model = ARIMA(data, order= order)
+    arima_fit =  model.fit()
+
+    return arima_fit
